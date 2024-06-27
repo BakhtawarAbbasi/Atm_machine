11d43337b4e7020cfda2b5fc4220cfd2b81625f9
BakhtawarAbbasi <bakhtawarabbasi009@gmail.com>
2024-06-27 12:24:06 +0500

    Atm machine

--- index.js
+++ index.js
@@ -1,2 +1,84 @@
 #! /usr/bin/env node
-export {};
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "input",
+        name: "userId",
+        message: "Kindly Enter Your ID",
+    },
+    {
+        type: "number",
+        name: "userPin",
+        message: "Kindly Enter Your PIN:"
+    },
+    {
+        type: "list",
+        name: "accountType",
+        choices: ["Current", "Saving", "Current Balance"],
+        message: "Please Select Your Account Type:",
+    },
+    {
+        type: "list",
+        name: "transactionType",
+        choices: ["Fast Cash", "Withdrawal", "Trasfer"],
+        message: "Select Your Transaction Type",
+        when(answers) {
+            return answers.accountType === "Current";
+        }
+    },
+    {
+        type: "list",
+        name: "transactionType",
+        choices: ["Fast Cash", "Withdrawal", "Transfer"],
+        message: "Select Your Transaction Type",
+        when(answers) {
+            return answers.accountType === "Saving";
+        }
+    },
+    {
+        type: "list",
+        name: "transactionType",
+        choices: ["Fast Cash", "Withdrawal", "Transfer"],
+        message: "Select Your Transaction Type",
+        when(answers) {
+            return answers.accountType === "Current Balance";
+        }
+    },
+    {
+        type: "number",
+        name: "amount",
+        choices: [1000, 2000, 10000, 20000],
+        message: "Select Your Amount",
+        when(answers) {
+            return answers.transactionType === "Fast Cash";
+        }
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter the Withdrawal Amount:",
+        when(answers) {
+            return answers.transactionType === "Withdrawal";
+        }
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter the Transfer Amount:",
+        when(answers) {
+            return answers.transactionType === "Transfer";
+        }
+    },
+]);
+if (answers.userId && answers.userPin) {
+    const balance = Math.floor(Math.random() * 1000000);
+    console.log("Current Balance");
+    const enteredAmount = answers.amount;
+    if (balance <= enteredAmount) {
+        console.log("Insufficient Balance");
+    }
+    else {
+        const remaining = balance - enteredAmount;
+        console.log("Your remaining balance is", remaining);
+    }
+}
